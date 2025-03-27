42f80b02bcd1bd1399f440d7834a120b479698c1
ILostTheGameAgain <alec.george@ucas-edu.net>
2025-03-27 13:44:07 -0600

    did things

--- visual.py
+++ visual.py
@@ -5,9 +5,16 @@
 #for easier access to data, use pandas
 import pandas as pd
 
+#all about graphs, needs matplotlib
+import matplotlib as mpl
+
 
 #main function to choose what to do
 def main():
     #variable to save money data
     df = pd.read_csv('users.csv').to_dict()
+
+
+    mpl.pie([1,2,3,4])
+    mpl.show()
 main()
