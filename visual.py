aaa73d86f5cb124fc1a9f36f1fdff7461b2d7e40
ILostTheGameAgain <alec.george@ucas-edu.net>
2025-03-27 13:38:42 -0600

    started work, saved info as a pandas dataframe/dictionary

--- visual.py
+++ visual.py
@@ -1,3 +1,13 @@
 # Alec George, Visuals
-def graph_menu():
-    pass
+
+#file for displaying graphs and visualizing data
+
+#for easier access to data, use pandas
+import pandas as pd
+
+
+#main function to choose what to do
+def main():
+    #variable to save money data
+    df = pd.read_csv('users.csv').to_dict()
+main()
